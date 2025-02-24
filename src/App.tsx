4f2fdd0787c406dabd1e8da87b4418b235645913
willrockies <wlsn.ads@gmail.com>
2025-02-24 14:47:36 -0300

    fixing some deploys bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import GlobalStyle from "./styles/global";
 
 import SignIn from './pages/Signin';
-import SignUp from './pages/Signup';
+//import SignUp from './pages/Signup';
 const App: React.FC = () => (
   <>
     <SignIn />
